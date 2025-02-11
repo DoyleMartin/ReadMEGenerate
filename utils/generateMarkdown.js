b12050a88f7bf7ba68d3117a63d2beee12f01729
DoyleMartin <Doylecmartin@gmail.com>
2025-02-11 18:26:10 -0500

    Rendering license and bones for readMe

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,16 +8,61 @@ function renderLicenseBadge(license) {
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None')
+    return '';
+  if (license === 'MIT') {
+    return '(https://opensource.org/licenses/MIT)';
+  }
+  if (license === 'Apache 2.0') {
+    return '(https://opensource.org/licenses/Apache-2.0)';
+  }
+  if (license === 'GPL 3.0') {
+    return '(https://opensource.org/licenses/GPL-3.0)';
+  }
+  if (license === 'BSD 3') {
+    return '(https://opensource.org/licenses/BSD-3-Clause)';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None')
+    return '';
+  if (license === 'MIT') {
+    return 'This project is licensed under the MIT license.';
+  }
+  if (license === 'Apache 2.0') {
+    return 'This project is licensed under the Apache 2.0 license.';
+  }
+  if (license === 'GPL 3.0') {
+    return 'This project is licensed under the GPL 3.0 license.';
+  }
+  if (license === 'BSD 3') {
+    return 'This project is licensed under the BSD 3 license.';
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description
 
+## Table of Contents
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
 `;
 }
 
